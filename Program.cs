9e8d168c9c5f0db3aebd33ef9b2adfc7cfb70c8c
bahareh smaili <bahareh_smaili_1990@yahoo.com>
2023-07-08 02:26:26 +0330

    add sample

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using HandlingAsynchronousExceptions;
+
+Console.WriteLine("Created by : Bahareh.Smi");
+Console.WriteLine("Date : 1402/04/15");
+Console.WriteLine();
+Console.WriteLine("-----------------------------------------------------------------------------");
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Exceptions in asynchronous methods that return void :");
+Console.WriteLine();
+
+sampleTaskThrowMultipleExceptionsAsync.ThisIsATestMethod();
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Exceptions in asynchronous methods that return a Task object :");
+Console.WriteLine();
+
+var result = sampleTaskThrowMultipleExceptionsAsync.ExceptionInAsyncCodeDemo();
+Console.WriteLine(result.Exception);
+Console.WriteLine(result.Status);
+
+Console.WriteLine();
+Console.WriteLine("Use the Exceptions property to retrieve all exceptions :");
+Console.WriteLine();
+
+var _result = sampleTaskThrowMultipleExceptionsAsync.ExceptionInAsyncAllCodeDemo();
+Console.WriteLine(_result.Exception);
+Console.WriteLine(_result.Status);
+Console.WriteLine();
+
+
+Console.ReadLine();
+Console.ReadLine();
+
---
+++ sampleTaskThrowMultipleExceptionsAsync.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingAsynchronousExceptions
+{
+    public class sampleTaskThrowMultipleExceptionsAsync
+    {
+        public static void ThisIsATestMethod()
+        {
+            try
+            {
+                AsyncMethodReturningVoid();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static async void AsyncMethodReturningVoid()
+        {
+            throw new Exception("This is an error message...");
+        }
+        //--------------------------------------------
+        public static async Task ExceptionInAsyncCodeDemo()
+        {
+            try
+            {
+                var task1 = Task.Run(() => throw new IndexOutOfRangeException
+                   ("IndexOutOfRangeException is thrown."));
+                var task2 = Task.Run(() => throw new ArithmeticException
+                   ("ArithmeticException is thrown."));
+                await Task.WhenAll(task1, task2);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //--------------------------------------------
+        public static async Task ExceptionInAsyncAllCodeDemo()
+        {
+            Task tasks = null;
+            try
+            {
+                var task1 = Task.Run(() => throw new IndexOutOfRangeException
+                   ("IndexOutOfRangeException is thrown."));
+                var task2 = Task.Run(() => throw new
+                   ArithmeticException("ArithmeticException is thrown."));
+                tasks = Task.WhenAll(task1, task2);
+                await tasks;
+            }
+            catch
+            {
+                AggregateException aggregateException = tasks.Exception;
+                foreach (var e in aggregateException.InnerExceptions)
+                {
+                    Console.WriteLine(e.GetType().ToString());
+                }
+            }
+        }
+    }
+}
